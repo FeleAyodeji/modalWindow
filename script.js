ef0e89be0142f49cc4a830f5afb75fcfed666d00
FeleAyodeji <feledeji@gmail.com>
2023-01-18 09:50:36 +0100

    fixed some bug

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const btnCloseModal = document.querySelector('.close-Modal')
 const btnsOpenModal = document.querySelectorAll('.show-Modal') //we used querySelectorAll to select the three open modal buttons.
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].addEventListener('click',function(){
+for (let i = 0; i < btnsOpenModal.length; i++) 
+    btnsOpenModal[i].addEventListener('click',function(){
         console.log('button clicked');
         modal.classList.remove('hidden')
-    }))    
-}
+        overlay.classList.remove('hidden')
+    })     
